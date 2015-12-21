711053054ff855d00c37d3f44feb17fa00366945
Jeffrey Camealy <jeffrey@theworklog.com>
2015-12-21 17:41:03 -0500

    code cleanup

---
+++ Moving Letters/Framework.swift
@@ -0,0 +1,7 @@
+
+
+import UIKit
+
+func p(x: Double,_ y: Double) -> CGPoint {
+    return CGPoint(x: x, y: y)
+}
--- Moving Letters/SourceCode/LetterView.swift
+++ Moving Letters/SourceCode/LetterView.swift
@@ -1,56 +1,8 @@
-//
-//  LetterView.swift
-//  Moving Letters
-//
-//  Created by Jeffrey Camealy on 12/16/15.
-//  Copyright © 2015 New Engineer. All rights reserved.
-//
+
+
+
 
 import UIKit
-
-func p(x: Double,_ y: Double) -> CGPoint {
-    return CGPoint(x: x, y: y)
-}
-
-func lineLayerWithPath(path: CGPath) -> CAShapeLayer {
-    let shapeLayer = CAShapeLayer()
-    shapeLayer.path = path
-    shapeLayer.strokeColor = UIColor.whiteColor().CGColor
-    shapeLayer.fillColor = UIColor.clearColor().CGColor
-    shapeLayer.lineWidth = 3
-    shapeLayer.lineCap = "round"
-    
-    return shapeLayer;
-}
-
-func strokeLayersForLetter(letter: Letter) -> [CAShapeLayer] {
-    var strokeLayers = [CAShapeLayer]()
-    for stroke in letter.strokes {
-        let path = UIBezierPath()
-        
-        if let lineSegment = stroke as? LineSegment {
-            path.moveToPoint(lineSegment.start)
-            path.addLineToPoint(lineSegment.end)
-        }
-        
-        if let bezierPath = stroke as? BezierPath {
-            path.moveToPoint(bezierPath.start)
-            var previousPoint = bezierPath.start
-            for bezierPoint in bezierPath.points {
-                let controlPoint1 = bezierPoint.controlPoint1 == nil ? previousPoint : bezierPoint.controlPoint1!
-                let controlPoint2 = bezierPoint.controlPoint2 == nil ? bezierPoint.point : bezierPoint.controlPoint2!
-                path.addCurveToPoint(bezierPoint.point, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
-                previousPoint = bezierPoint.point
-            }
-        }
-        
-        let layer = lineLayerWithPath(path.CGPath)
-        strokeLayers.append(layer)
-    }
-    
-    return strokeLayers;
-}
-
 
 
 class LetterView: UIView {
@@ -59,9 +11,6 @@ class LetterView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-//        self.backgroundColor = .redColor()
-        
-//        addLetter()
     }
     
     //MARK: - Public
@@ -120,20 +69,9 @@ class LetterView: UIView {
                 animation.fromValue = strokeLayer.path
                 strokeLayer.path = path
                 animation.toValue = path
-//                animation.removedOnCompletion = false
-//                animation.fillMode = kCAFillModeForwards
                 strokeLayers[i].addAnimation(animation, forKey: "path")
                 
                 newStrokeLayers.append(strokeLayers[i])
-                
-//                let animation = CABasicAnimation(keyPath: "path")
-//                animation.duration = animationDuration
-//                animation.fromValue = segmentLayer.path
-//                segmentLayer.path = newPath
-//                animation.toValue = newPath
-//                animation.removedOnCompletion = false
-//                animation.fillMode = kCAFillModeForwards
-//                segmentLayer.addAnimation(animation, forKey: "path")
             }
         }
         
@@ -142,57 +80,22 @@ class LetterView: UIView {
     
     //MARK: - Private
     
+    // Vars
+    
     var strokeLayers = [CAShapeLayer]()
     let animationDuration = 2.0
     
-    func addLetter() {
-//        // Path 1
-//        let path1 = UIBezierPath()
-//        path1.moveToPoint(p(30,0))
-//        path1.addCurveToPoint(p(40,40), controlPoint1: p(0,0), controlPoint2: p(-10,60))
-//        
-//        // Path 2
-//        let path2 = UIBezierPath()
-//        path2.moveToPoint(p(0,0))
-//        path2.addLineToPoint(p(0,50))
+    // Methods
+    
+    func lineLayerWithPath(path: CGPath) -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path
+        shapeLayer.strokeColor = UIColor.whiteColor().CGColor
+        shapeLayer.fillColor = UIColor.clearColor().CGColor
+        shapeLayer.lineWidth = 3
+        shapeLayer.lineCap = "round"
         
-//        let aLayers = strokeLayersForLetter(A)
-        let bLayers = strokeLayersForLetter(B)
-        
-        // Shape Layer
-        for strokeLayer in bLayers {
-            layer.addSublayer(strokeLayer)
-            
-            let animation = CABasicAnimation(keyPath: "strokeEnd")
-            animation.duration = 2
-            animation.fromValue = 0
-            animation.toValue = 1
-            strokeLayer.addAnimation(animation, forKey: "strokeEnd")
-        }
-        
-        
-        //
-//        for (i, strokeLayer) in aLayers.enumerate() {
-//            if i >= bLayers.count {
-//                continue
-//            }
-//            
-//            let animation = CABasicAnimation(keyPath: "path")
-//            animation.duration = 2
-//            animation.toValue = strokeLayer.path
-//            animation.removedOnCompletion = false
-//            animation.fillMode = kCAFillModeForwards
-//            bLayers[i].addAnimation(animation, forKey: "path")
-//        }
-
-        
-        // Animation
-//        let animation = CABasicAnimation(keyPath: "path")
-//        animation.duration = 2
-//        animation.toValue = path2.CGPath
-//        animation.removedOnCompletion = false
-//        animation.fillMode = kCAFillModeForwards
-//        strokeLayers[0].addAnimation(animation, forKey: "path")
+        return shapeLayer;
     }
     
     func pathForStroke(stroke: Any) -> CGPath {
